929e636bbea98325ae7e8b60385828208db2d625
Matt Blubaugh <mblubaugh@ofbf.org>
2016-12-29 09:05:09 -0500

    Add tests
    Setup mocha and chai and run a couple basic tests. Also create automatic test runner.

---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+// Test with an asynchronous callback
+// must have the done paramater passed and executed for accurate test result
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal('Hello World!');
+      done(); // must call this to let mocha know async has completed
+      window.close();
+    });
+  });
+});
