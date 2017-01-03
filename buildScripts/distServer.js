b12c210862891f3fd7ab2432cd5a7991f5dc3d3b
Matt Blubaugh <mblubaugh@ofbf.org>
2017-01-02 21:35:14 -0500

    Update production endpoint
    Use the heroku test app endpoint

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -20,14 +20,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req, res) {
-  // hard coded for simplicity. pretend this hits a real database
-  res.json([
-    {"id":1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
-    {"id":2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
-    {"id":3, "firstName":"Tina", "lastName":"Lee", "email":"lee.tina@hotmail.com"}
-  ])
-});
+// updated to use heroku test app https://enigmatic-everglades-55826.herokuapp.com/
+// app.get('/users', function(req, res) {
+//   // hard coded for simplicity. pretend this hits a real database
+//   res.json([
+//     {"id":1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+//     {"id":2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
+//     {"id":3, "firstName":"Tina", "lastName":"Lee", "email":"lee.tina@hotmail.com"}
+//   ])
+// });
 
 app.listen(port, function(err) {
   if (err) {
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl() {
   // const inDevelopment = window.location.hostname === 'localhost';
   // return inDevelopment ? 'http://localhost:3001/' : '/';
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://enigmatic-everglades-55826.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
